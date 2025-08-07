6ece2a88f94b49992b5ab33c9ce47eeff2bf87fd
Birostech <olayemisamuel5@gmail.com>
2025-08-07 01:34:14 +0100

    my terraform and cicd project

--- variable.tf
+++ variable.tf
@@ -15,47 +15,47 @@ variable "instance_type" {
 variable "private_subnet_cidr" {
   description = "private subnet cidr block"
   type        = string
-  sensitive     = true
+  sensitive   = true
 }
 
 variable "private_subnet_cidr_2" {
   description = "private subnet 2 cidr block"
   type        = string
-  sensitive     = true
+  sensitive   = true
 }
 
 variable "rds_port" {
   description = "ports for the rds"
   type        = number
-  sensitive     = true
+  sensitive   = true
 }
 
 variable "db_username" {
   description = "db username"
   type        = string
-  sensitive     = true
+  sensitive   = true
 }
 
 variable "db_password" {
   description = "db password"
   type        = string
-  sensitive     = true
+  sensitive   = true
 }
 
 variable "private_ec2_port" {
   description = "ports for the private ec2"
   type        = number
-  sensitive     = true
+  sensitive   = true
 }
 
 variable "private_ec2_port_2" {
   description = "ports for second private ec2 "
   type        = number
-  sensitive     = true
+  sensitive   = true
 }
 
 variable "key_name" {
   description = "ec2 key name"
   type        = string
-  sensitive     = true
+  sensitive   = true
 }
